0205bd4c2905415b4561beba0bb02c4bb8e08109
juancarlosTudela <juanktudela@gmail.com>
2022-03-02 18:37:22 -0500

    Allure Report

--- prueba_GhA/UnitTest1.cs
+++ prueba_GhA/UnitTest1.cs
@@ -1,9 +1,14 @@
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using prueba_GhA.Factories;
 
 namespace prueba_GhA
 {
+    [TestFixture]
+    [AllureNUnit]
     public class Tests
     {
         private IWebDriver driver;
@@ -12,6 +17,7 @@ namespace prueba_GhA
         [SetUp]
         public void Setup()
         {
+            AllureLifecycle.Instance.CleanupResultDirectory();
             hubUrl = "http://localhost:4444/wd/hub";
             driver = LocalDriverFactory.CreateInstance(Enum.BrowserType.Chrome, hubUrl);
         }
@@ -23,12 +29,22 @@ namespace prueba_GhA
         }
 
         [Test]
+        [Category("SampleTag")]
+        [Description("Test1")]
+        [AllureTag("NUnit", "Debug")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureFeature("Core")]
         public void Test1()
         {
             driver.Navigate().GoToUrl("https://www.google.com");
             driver.Manage().Window.Maximize();
             driver.FindElement(By.Name("q")).SendKeys("I Want to se this on a remote machine");
-            Assert.Pass();
+            AllureLifecycle.Instance.WrapInStep(() =>
+            {
+                Assert.Pass();
+                
+            }, "Google Search");
+            
         }
     }
 }
